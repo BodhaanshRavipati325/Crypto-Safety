986a7500298c3c600f34539ead7b7b364a09de2c
Bodhaansh Ravipati <45471123+BodhaanshRavipati325@users.noreply.github.com>
2023-07-14 11:26:02 -0500

    Add API Endpoints

--- server/server.js
+++ server/server.js
@@ -1,34 +1,99 @@
-const axios = require('axios');
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const axios = require("axios");
+
 app.use(cors());
 app.use(express.json());
 
-let config = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: 'https://open-api.coinglass.com/public/v2/liquidation_top?time_type=h4',
-  headers: { 
-    'coinglassSecret': 'e630792b64d1476c9d350efbe0ee54eb'
-  }
-};
-
-axios.request(config)
-.then((response) => {
-  liquidationData = response.data;
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
-
-let liquidationData = {};
+let liquidationData = [];
+let longShortRatio = [];
+let takerFees = [];
 
 app.get("/liquidation-data", (req, res) => {
-  res.json(liquidationData);
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://open-api.coinglass.com/public/v2/liquidation_ex?time_type=h4&symbol=all",
+    headers: {
+      coinglassSecret: "e630792b64d1476c9d350efbe0ee54eb",
+    },
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+
+      liquidationData = [];
+      let total = 0;
+      let average = 0;
+
+      for (let i = 1; i <= 6; i++) {
+        total += response.data.data[i].totalVolUsd;
+      }
+
+      average = total / 6;
+
+      for (let i = 1; i <= 6; i++) {
+        if (response.data.data[i].totalVolUsd > average) {
+          liquidationData.push({"volume": response.data.data[i].totalVolUsd, "color": "green", "name": response.data.data[i].exchangeName});
+        }
+        else {
+          liquidationData.push({"volume": response.data.data[i].totalVolUsd, "color": "red", "name": response.data.data[i].exchangeName});
+        }
+      }
+
+      res.json(liquidationData);
+    })
+    .catch((error) => {
+      console.log(error);
+      return "no data available";
+    });
+});
+
+app.get("/longShortRatio", (req, res) => {
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://open-api.coinglass.com/public/v2/long_short?time_type=h1&symbol=BTC",
+    headers: {
+      coinglassSecret: "e630792b64d1476c9d350efbe0ee54eb",
+    },
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+
+      longShortRatio = [];
+      let total = 0;
+      let average = 0;
+
+      for (let i = 0; i <= 5; i++) {
+        total += response.data.data[0].list[i].longRate / response.data.data[0].list[i].shortRate;
+      }
+
+      average = total / 6;
+
+      for (let i = 0; i <= 5; i++) {
+        let mRatio = response.data.data[0].list[i].longRate / response.data.data[0].list[i].shortRate;
+        if (mRatio > average) {
+          longShortRatio.push({"longShortRatio": mRatio, "color": "green", "name": response.data.data[0].list[i].exchangeName});
+        }
+        else {
+          longShortRatio.push({"longShortRatio": mRatio, "color": "red", "name": response.data.data[0].list[i].exchangeName});
+        }
+      }
+
+      res.json(longShortRatio);
+    })
+    .catch((error) => {
+      console.log(error);
+      return "no data available";
+    });
 });
 
 app.listen(8000, () => {
